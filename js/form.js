7ec61c30c505faec9c2774a9e964d89f90dd8f2f
benetta <ksenyatokareva@gmail.com>
2018-05-05 13:51:52 +0300

    добавила возможность загрузки собственной картинки

--- js/form.js
+++ js/form.js
@@ -1,6 +1,8 @@
 'use strict';
 
 (function () {
+  var FILE_TYPES = ['image/jpeg', 'image/jpg', 'image/png'];
+
   var formElement = document.querySelector('#upload-select-image');
 
   var parentEl = window.common.imageUploadElement;
@@ -8,6 +10,9 @@
 
   var uploadFile = document.querySelector('#upload-file');
   var uploadCancel = document.querySelector('#upload-cancel');
+
+  var inputOriginal = document.querySelector('#effect-none');
+
   var hashtagElement = document.querySelector('.text__hashtags');
   var commentsInput = parentEl.querySelector('.text__description');
 
@@ -20,34 +25,57 @@
   var scaleLevel = parentEl.querySelector('.scale__level');
   var scaleValue = parentEl.querySelector('.scale__value');
 
-  var clearForm = function (close) {
+  var clearForm = function () {
+    imagePreview.removeAttribute('style');
+    imagePreview.removeAttribute('class');
+    imagePreview.src = 'img/upload-default-image.jpg';
+
     resizeControlValue.value = '100%';
+
     imageSlider.classList.add('hidden');
     scalePin.style.left = '100%';
     scaleLevel.style.width = '100%';
     scaleValue.value = '100';
-    imagePreview.removeAttribute('style');
-    imagePreview.removeAttribute('class');
-    hashtagElement.value = '';
-    commentsInput.value = '';
-    hashtagElement.style = null;
 
-    if (close) {
-      parentEl.classList.add('hidden');
-      uploadFile.value = '';
-    }
+    inputOriginal.checked = 'true';
+
+    hashtagElement.style = null;
+    hashtagElement.value = '';
+
+    commentsInput.value = '';
+
+    uploadFile.removeAttribute('value');
+    parentEl.classList.add('hidden');
   };
 
   var onUploadFileClick = function () {
-    clearForm(false);
+    imageSlider.classList.add('hidden');
     parentEl.classList.remove('hidden');
     resizeControl.style.zIndex = 1;
 
     document.addEventListener('keydown', onUploadElementEscPress);
+
+    var file = uploadFile.files[0];
+
+    var isMatch = FILE_TYPES.some(function () {
+      return file.type;
+    });
+
+    if (isMatch) {
+      var reader = new FileReader();
+
+      reader.addEventListener('load', function () {
+        imagePreview.src = reader.result;
+        imagePreview.width = '586';
+        imagePreview.removeAttribute('height');
+      });
+
+      reader.readAsDataURL(file);
+    }
   };
 
   var onUploadCancelClick = function () {
-    clearForm(true);
+    clearForm();
 
     document.removeEventListener('keydown', onUploadElementEscPress);
   };
@@ -62,7 +90,7 @@
   uploadCancel.addEventListener('click', onUploadCancelClick);
 
   var onLoad = function () {
-    clearForm(true);
+    clearForm();
   };
 
   var errorElement = document.querySelector('.img-upload__message--error');
